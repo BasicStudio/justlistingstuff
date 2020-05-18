fe060eb5cba70bf2987dd40ba14dee98ff7cbc37
Javascriptgold <53185933+BasicStudio@users.noreply.github.com>
2020-05-18 14:21:09 -0700

    Update choose.bat

--- choose.bat
+++ choose.bat
@@ -1,35 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 :begin
 set /p op=Type option:
@@ -59,12 +27,12 @@ goto begin
 
 :op3
 echo Like winRAR but you wanna go pro
-bitsadmin /transfer wcb /priority high https://cdn.discordapp.com/attachments/711474825557966850/711712597505867826/Screenshot2.jpg %CD%/winRAR.zip
+bitsadmin /transfer wcb /priority high https://github.com/BasicStudio/justsomeart/raw/master/Screenshot2.jpg %CD%/winRAR.zip
 goto begin
 
 :op4
 echo Keeping track of your computer huh
-bitsadmin /transfer wcb /priority high https://cdn.discordapp.com/attachments/711474825557966850/711794929608687667/Screenshot3.jpg %CD%/HDTune.zip
+bitsadmin /transfer wcb /priority high https://github.com/BasicStudio/justsomeart/raw/master/Screenshot3.jpg %CD%/HDTune.zip
 goto begin
 
 :op5
@@ -74,7 +42,7 @@ goto begin
 
 :op6
 echo Lost the password to your archive huh
-bitsadmin /transfer wcb /priority high https://cdn.discordapp.com/attachments/711474825557966850/711796712854913104/Screenshot5.jpg %CD%/AARPPE.7z
+bitsadmin /transfer wcb /priority high https://github.com/BasicStudio/justsomeart/raw/master/Screenshot5.jpg %CD%/AARPPE.7z
 goto begin
 
 :op7
